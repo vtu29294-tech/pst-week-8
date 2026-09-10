be4da1bdf6dace637810ae7349d403e7208a1264
vtu29294-tech <vtu29294@veltech.edu.in>
2026-09-10 17:59:23 +0530

    Create BiCycle.java

---
+++ BiCycle.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Cycle {
+    String define_me() {
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends Cycle {
+    String define_me() {
+        return "a cycle with an engine.";
+    }
+
+    MotorCycle() {
+        System.out.println("Hello I am a motorcycle, I am " + define_me());
+        String temp = super.define_me();
+        System.out.println("My ancestor is a cycle who is " + temp);
+    }
+}
+
+public class BiCycle {
+    public static void main(String []args) {
+        MotorCycle M = new MotorCycle();
+    }
+}
